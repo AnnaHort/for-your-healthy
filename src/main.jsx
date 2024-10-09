381dec2504d158e73a25dd876f5fb6af6aef21f4
AnnaHort <annahort3d@gmail.com>
2024-10-10 00:17:24 +0300

    update route in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import App from "../src/App";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
